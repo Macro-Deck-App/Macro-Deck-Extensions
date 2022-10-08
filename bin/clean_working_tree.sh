0c7b33f65fef3db2abd234b628f375829a967262
RecklessBoon <16234384+RecklessBoon@users.noreply.github.com>
2022-10-08 14:27:33 -0500

    Create clean_working_tree.sh

---
+++ bin/clean_working_tree.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git clean -xfd
+git submodule foreach --recursive git clean -xfd
+git reset --hard
+git submodule foreach --recursive git reset --hard
+git submodule update --init --recursive
