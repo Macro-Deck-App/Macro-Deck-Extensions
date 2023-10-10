e29d3d2f77c55d845071b3a7a60a2947cac3842f
Manuel Mayer <45011179+manuelmayer-dev@users.noreply.github.com>
2023-10-10 07:44:03 +0200

    Delete bin/clean_working_tree.sh

--- bin/clean_working_tree.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-git clean -xfd
-git submodule foreach --recursive git clean -xfd
-git reset --hard
-git submodule foreach --recursive git reset --hard
-git submodule update --init --recursive
